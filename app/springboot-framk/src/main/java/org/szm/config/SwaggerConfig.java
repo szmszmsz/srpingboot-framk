d08267d3f13684d2184bfcabc4cf551ddf79b551
shizhaoming <shizhaoming@kingsware.cn>
2024-05-04 22:02:10 +0800

    swagger集成

---
+++ app/springboot-framk/src/main/java/org/szm/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package org.szm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableWebMvc
+@EnableSwagger2 // 开启Swagger2
+
+public class SwaggerConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+http://localhost:8082/api/swagger-ui.html
